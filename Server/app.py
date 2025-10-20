f975ae76ab887e4d6fe992d76d93d9ce628bb999
Elle Christie <AChristie06@protonmail.com>
2025-10-20 22:50:34 +1030

    minor refactor

--- Server/app.py
+++ Server/app.py
@@ -1,11 +1,9 @@
 import os
-import requests
 from flask import Flask, Response, make_response, request, abort, jsonify
 from flask_cors import CORS
 import googleapiclient.discovery
 from googleapiclient.errors import HttpError
-import bandcamp_api
-from bandcamp_api import Bandcamp
+from Server.Vendored.bandcamp_api import Bandcamp
 
 app = Flask(__name__)
 CORS(app)
@@ -22,63 +20,57 @@ bc = Bandcamp()
 
 @app.route("/getplaylist", methods=["GET"])
 def getplaylistitems():
-    def id_to_videos(playlist_id):
-        def items_to_video_objects(list_of_items):
-            return [
-                {
-                    "type": "youtubeVideo",
-                    "title": item["snippet"]["title"],
-                    "videoId": item["contentDetails"]["videoId"],
-                }
-                for item in list_of_items
-            ]
-
-        try:
-            the_request = youtube.playlistItems().list(
-                part="snippet,contentDetails",
-                maxResults=50,
-                playlistId=playlist_id,
-                fields="nextPageToken, items(contentDetails/videoId,snippet/title)",
-            )
-            the_response = the_request.execute()
-        except HttpError as err:
-            errargs = err.args[0]
-            errcode = errargs["status"]
-
-            if errcode == "404" or errcode == "400":
-                the_response = jsonify(
-                    {"err": "Playlist Not Found. Is the URL correct?"}
-                )
-                the_response.status = 400
-                abort(the_response)
-            elif errcode == "403":
-                the_response = jsonify(
-                    {
-                        "err": "Playlist not accessable. This app only supports importing public playlists."
-                    }
-                )
-                the_response.status = 403
-                abort(the_response)
-            else:
-                the_response = jsonify({"err": "Unknown error."})
-                the_response.status = 500
-                abort(the_response)
-        else:
-            list_of_videos = items_to_video_objects(the_response["items"])
-            while "nextPageToken" in the_response:
-                the_request = youtube.playlistItems().list_next(
-                    the_request, the_response
-                )
-                the_response = the_request.execute()
-                list_of_videos = list_of_videos + items_to_video_objects(
-                    the_response["items"]
-                )
-            return list_of_videos
+    def items_to_video_objects(list_of_items):
+        return [
+            {
+                "type": "youtubeVideo",
+                "title": item["snippet"]["title"],
+                "videoId": item["contentDetails"]["videoId"],
+            }
+            for item in list_of_items
+        ]
 
     playlist_id = request.args.get("playlistID")
     if playlist_id is None:
         abort(400)
-    return id_to_videos(playlist_id)
+
+    try:
+        the_request = youtube.playlistItems().list(
+            part="snippet,contentDetails",
+            maxResults=50,
+            playlistId=playlist_id,
+            fields="nextPageToken, items(contentDetails/videoId,snippet/title)",
+        )
+        the_response = the_request.execute()
+    except HttpError as err:
+        errargs = err.args[0]
+        errcode = errargs["status"]
+
+        if errcode == "404" or errcode == "400":
+            the_response = jsonify({"err": "Playlist Not Found. Is the URL correct?"})
+            the_response.status = 400
+            abort(the_response)
+        elif errcode == "403":
+            the_response = jsonify(
+                {
+                    "err": "Playlist not accessable. This app only supports importing public playlists."
+                }
+            )
+            the_response.status = 403
+            abort(the_response)
+        else:
+            the_response = jsonify({"err": "Unknown error."})
+            the_response.status = 500
+            abort(the_response)
+    else:
+        list_of_videos = items_to_video_objects(the_response["items"])
+        while "nextPageToken" in the_response:
+            the_request = youtube.playlistItems().list_next(the_request, the_response)
+            the_response = the_request.execute()
+            list_of_videos = list_of_videos + items_to_video_objects(
+                the_response["items"]
+            )
+        return list_of_videos
 
 
 @app.route("/getbandcampalbum", methods=["GET"])
